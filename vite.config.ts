b03b74d41e38a184f8190b3778ecdf5254431933
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2024-01-29 15:43:53 -0800

    clean up config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    resolve: {
-        alias: {
-            "@croquet/react": path.resolve(__dirname, "node_modules/@croquet/react/index.ts")
-        }
-    },
-    base: "./"
+    base: './',
+    build: {
+        minify: false
+    }
 })
